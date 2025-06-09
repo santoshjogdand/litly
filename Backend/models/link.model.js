dc5d246a8fa5b5180a3c9efe6f0bf0c84dba1783
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-09 20:03:23 +0530

    Added link model to store shortned urls

---
+++ Backend/models/link.model.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const LinkSchema = Schema({
+    originalUrl: { 
+        type: String,
+        required: true
+    },
+    shortCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    clickCount:{
+        type: Number,
+        default: 0
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    }
+})
+
+
+export default model("Link", LinkSchema)
