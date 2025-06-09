8e76dc78167522a8ddaa9e37152bb5e60c4af4b3
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-09 19:50:48 +0530

    Added method for password hashing, hash comaprison and auth token genration

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema({
     username:{
@@ -19,9 +21,52 @@ const UserSchema = new Schema({
     urls: [{
         type: Schema.Types.ObjectId,
         ref: "Link"
-    }]
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    },
+    // isSubsscribed: {
+    //     type: Boolean,
+    //     default: false
+    // },
+    // subscription:{
+    //     type: Schema.Types.ObjectId,
+    //     ref: ""
+    // }
 });
 
+UserSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this,this.password, 10);
+    }
+    if(this.isModified()){
+        this.updatedAt = Date.now()
+    }
+    next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = async function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 export default model("User", UserSchema);
