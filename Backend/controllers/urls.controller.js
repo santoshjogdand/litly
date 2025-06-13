14c44c6cdc07ec9d5a46d0e56d2ffd5c2d83bb15
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-13 22:30:54 +0530

    Controller to manage urls

---
+++ Backend/controllers/urls.controller.js
@@ -0,0 +1,90 @@
+import Analytics from "../models/analytic.model.js";
+import asyncHandler from "../utils/AsyncHandler.js";
+import Link from "../models/link.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import generateShotCode from "../utils/generateShortCode.js";
+
+const getUrls = asyncHandler(async (req, res) => {
+    const userid = req.userid;
+    const urls = await Link.find({
+        createdBy: userid
+    }).select("-createdBy")
+    if (!urls) {
+        throw new ApiError(401, "No short urls found for the user")
+    }
+    res.status(200).json(new ApiResponse(urls, "Urls found"))
+})
+const getUrlAnalytic = asyncHandler(async (req, res) => {
+    const userid = req.userid
+    const urlid = req.params.id
+    const urlFound = await Link.findOne({
+        createdBy: userid,
+        _id: urlid
+    }).select("_id")
+    if (!urlFound) {
+        throw new ApiError(404, "Url not found for user")
+    }
+    const url = await Analytics.find({
+        link: urlid
+    });
+    if (!url) {
+        throw new ApiError(409, "Analytics not found!")
+    }
+    res.status(200).json(new ApiResponse(url, "Analytics data found"))
+})
+const deleteUrl = asyncHandler(async (req, res) => {
+    const userid = req.userid
+    const urlid = req.params.id
+    const urlFound = await Link.findOne({
+        createdBy: userid,
+        _id: urlid
+    });
+    if (!urlFound) {
+        throw new ApiError(409, "Url doesnt exists")
+    }
+    const analyticData = await Analytics.findOne({
+        Link: urlFound._id
+    })
+    if (analyticData) {
+        await analyticData.remove()
+    }
+    await urlFound.remove()
+
+    res.status(200).json(new ApiResponse({}, "Shortned url removed"))
+
+})
+
+const updateUrl = asyncHandler(async (req, res) => {
+    const userid = req.userid;
+    const urlid = req.params.id;
+    let shortCode = req.body.customUrl || false;
+
+    const urlFound = await Link.findOne({
+        createdBy: userid,
+        _id: urlid
+    });
+
+    if (!urlFound) {
+        throw new ApiError(404, "URL doesn't exist");
+    }
+    let shortCodeExist;
+    if (shortCode) {
+        shortCodeExist = await Link.findOne({ shortCode });
+        if(shortCodeExist){
+            throw new ApiError(409, "Requested custom url is already taken")
+        }
+    } else {
+        do {
+            shortCode = generateShotCode();
+            shortCodeExist = await Link.findOne({ shortCode });
+        } while (shortCodeExist);
+    }
+
+    urlFound.shortCode = shortCode;
+    await urlFound.save();
+
+    res.status(200).json(new ApiResponse(urlFound, "Shortcode updated successfully"));
+
+})
+export { getUrls, getUrlAnalytic, deleteUrl, updateUrl }
