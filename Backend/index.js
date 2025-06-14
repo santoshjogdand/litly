6adcc678ae5f88493f92566e6b2cddffb9e46d6d
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-14 19:19:10 +0530

    Some changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,11 @@
 //Server startup & listening
 import app from "./app.js";
 import dotenv from "dotenv";
-import chalk from "chalk"
 import connectDB from "./config/db.js";
 dotenv.config();
 const PORT = process.env.PORT || 8000
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(chalk.green(`🚀 Server running at http://localhost:${PORT}`));
+        console.log(`🚀 Server running at port: ${PORT}`);
     })
 })
