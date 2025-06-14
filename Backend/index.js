47e8de6a4fca495878889b2efc108fbbbe4e4f6a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-14 19:20:40 +0530

    Added server config changes

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-dotenv.config();
+dotenv.config({
+    path: './.env'
+})
 const PORT = process.env.PORT || 8000
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`🚀 Server running at port: ${PORT}`);
     })
-})
+}).catch((err)=>console.log("Error occured: "), err)
