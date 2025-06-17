d127da91abbfb0214fd58babaa5f8c9b0e82ab56
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-17 12:00:00 +0530

    Setup app routing and structure

--- Frontend/litly-frontend/src/App.jsx
+++ Frontend/litly-frontend/src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import PublicLayout from './Layout/PublicLayout';
+import PrivateLayout from './Layout/PrivateLayout';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Signup from './pages/SignUp'
+import Dashboard from './pages/Dashboard';
+import Analytics from './pages/Analytics';
+import About from './pages/About';
+import Links from './pages/Links';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='font-semibold'>Initial fixed react installation with tailwind</h1> 
-    </>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes >
+          <Route element={<PublicLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/Signup' element={<Signup />} />
+            <Route path='/About' element={<About />} />
+          </Route>
+
+          <Route path="/Dashboard" element={<PrivateLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="Links" element={<Links />} />
+            <Route path="Analytics" element={<Analytics />} />
+          </Route>
+        </Routes> 
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
