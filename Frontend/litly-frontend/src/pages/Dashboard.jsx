f30a04cea5d99f5d4be6e405b9a446f12451f64d
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-17 12:00:00 +0530

    Add Dashboard dashboard page

---
+++ Frontend/litly-frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Dashboard() {
+  return (
+    <div className='text-black'>Dashboard</div>
+  )
+}
+
+export default Dashboard
