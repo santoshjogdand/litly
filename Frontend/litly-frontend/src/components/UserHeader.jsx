27f275205eb4da3441e4d016a30003355c8dc471
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-20 23:02:55 +0530

    added hover effects to button and changed button color

--- Frontend/litly-frontend/src/components/UserHeader.jsx
+++ Frontend/litly-frontend/src/components/UserHeader.jsx
@@ -6,7 +6,7 @@ function UserHeader() {
       <nav className='h-18 fixed flex items-center gap-10 justify-end px-10 left-40 right-0 w-auto py-9'>
         <input className=' w-80 h-full p-5 bg-[#a455cf] text-white outline-none rounded-3xl' placeholder='Search'/>
         <div className='flex gap-4 items-center justify-centerp-0 m-0 '>
-          <button className='h-[40px] w-[8rem] text-center bg-[#a455cf] outline-none rounded-md border-1 border-black font-medium  shadow-[0px_3px_0px_0px_rgba(0,0,0)] flex items-center justify-center gap-1 px-2 text-white'><HandHeart className='stroke-[1.5px]' />Donate</button>
+          <button className='h-[40px] w-[8rem] text-center bg-purple-500 outline-none rounded-md border-1 border-black font-medium  shadow-[0px_3px_0px_0px_rgba(0,0,0)] flex items-center justify-center gap-1 px-2 text-white hover:bg-purple-600 cursor-pointer'><HandHeart className='stroke-[1.5px]' />Donate</button>
           <div className="userprofile flex items-center gap-3">
             <span className="w-[40px] h-[40px] rounded-full border-1 border-black text-center flex items-center justify-center text-xl">
               <User2Icon className='stroke-[1.5px]' />
