c7e60f4751e66645ccf70145f3b4f7cce649e52a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-20 22:39:19 +0530

    Adding dashboard

--- Frontend/litly-frontend/src/pages/Dashboard.jsx
+++ Frontend/litly-frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Flame } from 'lucide-react';
+import React, { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 function Dashboard() {
+  const [hostname, setHostname] = useState('');
+
+  useEffect(() => {
+    // Access window.location.hostname within a client-side context
+    if (typeof window !== 'undefined') {
+      setHostname(window.location.hostname);
+    }
+  }, []); // Empty dependency array ensures this runs once after initial render
+
   return (
-    <div className='text-black h-[200vh] overflow-auto scrollbar-hide'>Dashboard</div>
+    <div className='text-black h-full w-full overflow-auto flex justify-center items-center font-sans'>
+
+    </div>
   )
 }
 
