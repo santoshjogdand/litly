c8cc4600f33341441bf7d458a4b73511dbe63fdf
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-20 22:47:08 +0530

    Created modal component for custom short links

---
+++ Frontend/litly-frontend/src/components/CustomShortModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomShortModal = ({className}) => {
+  return (
+    <div className={className} >CustomShortModal</div>
+  )
+}
+
+export default CustomShortModal
