4ad0cfb72692e0839fb521e1e0ef864568d2678b
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-22 01:20:48 +0530

    Header and side bar mobile optimization

--- Frontend/litly-frontend/src/components/SideBar.jsx
+++ Frontend/litly-frontend/src/components/SideBar.jsx
@@ -25,15 +25,11 @@
         }
 
         return (
-            <nav id="side-nav" className={`h-[100dvh] fixed top-0 left-0 flex flex-col justify-center z-20 transition-all duration-300 ease-in-out ${status ? 'w-60' : 'w-20'}`} >
-                <div className="topContent flex justify-center h-21 items-center transition-all duration-300 ease-in-out ">
-                    <span id="logo-1" className="text-transparent bg-clip-text py-1 rounded font-bold text-2xl bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 w-fit inline-block">
-                        Litly
-                    </span>
-                </div>
-                <div className={`h-full mt-1 shadow-lg bg-white py-10 flex flex-col gap-6 transition-all duration-300 ease-in-out ${status ? 'w-full' : 'w-20'}`}>
+            <nav id="side-nav" className={`h-[100dvh] fixed top-0 left-0 flex flex-col justify-center transition-all duration-300 ease-in-out ${status ? 'w-60' : 'w-20'}`} >
+                
+                <div className={`h-full pt-30 mt-2 shadow-lg bg-white py-10 flex flex-col gap-6 transition-all duration-300 ease-in-out ${status ? 'w-full' : 'w-20'}`}>
 
-                    <span className='w-4 h-4 relative -mt-14 ml-auto mr-[-5px] transition-all ease-in-out duration-300'><CircleChevronLeft className={`bg-[#a455cf] rounded-full text-white transition-transform duration-300 ease-in-out ${status ? 'rotate-0' : 'rotate-180'}`} onClick={modifyStatus} /></span>
+                    <span className='w-4 z-10 h-4 relative -mt-14 ml-auto mr-[-5px] transition-all ease-in-out duration-300'><CircleChevronLeft className={`bg-[#a455cf] rounded-full text-white transition-transform duration-300 ease-in-out ${status ? 'rotate-0' : 'rotate-180'}`} onClick={modifyStatus} /></span>
                         <SideBarMenu elementArray={elements} status={status} />
                     <span className='bg-[#dbe0eb] w-auto h-[1px] mx-10'></span>
                 </div>
--- Frontend/litly-frontend/src/components/UserHeader.jsx
+++ Frontend/litly-frontend/src/components/UserHeader.jsx
@@ -1,25 +1,38 @@
-import { HandHeart, Search, User2Icon } from 'lucide-react';
+import { HandHeart, Search, User2Icon, Menu } from 'lucide-react';
 import { useState } from 'react';
 
 function UserHeader() {
+  const [menuStatus, setMenuStatus] = useState(false)
   const [value, setValue] = useState('');
-  const searchLink = () =>{
+  const openMenu = () => {
+    setMenuStatus(!menuStatus)
+  }
+  const searchLink = () => {
     console.log(value)
     alert(`Search query: ${value}`);
   }
   return (
-    <header className='w-full fixed z-10 h-18 border-b-1 border-[#dbe0eb] bg-white shadow-[0_4px_6px_-4px_rgba(0,0,0,0.3)]' >
-      <nav className='h-18 fixed flex items-center gap-10 justify-end px-10 left-40 right-0 w-auto py-9'>
-        <div className="text-white w-80 h-11 flex items-center justify-around px-4 gap-2 searchBar bg-purple-500 hover:shadow-purple-500 shadow rounded-3xl">
-          <input className=' outline-none w-full ' value={value} onChange={(e)=>setValue(e.target.value)}  placeholder='Search'/> <Search onClick={searchLink} />
+    <header className='w-full fixed md:z-10 z-20 h-16 border-b-1 border-[#dbe0eb] bg-white shadow-[0_4px_6px_-4px_rgba(0,0,0,0.3)]' >
+
+      <nav className='md:h-16 md:top-0 flex z-20 flex-col md:flex-row md:justify-between items-center md:px-10 md:w-auto md:py-9 bg-[white] md:bg-none shadow-lg '>
+        <div className="flex flex-row items-center px-5 justify-between w-full md:bg-none bg-white h-16 z-20 static topContent transition-all duration-300 ease-in-out ">
+          <span id="logo-1" className="text-transparent bg-clip-text md:py-1 rounded font-bold text-2xl bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 md:w-fit block">
+            Litly
+          </span>
+          <Menu onClick={openMenu} className=' md:opacity-0 z-20' />
         </div>
-        <div className='flex gap-4 items-center justify-centerp-0 m-0 '>
-          <button className='h-[40px] w-[8rem] text-center bg-purple-500 outline-none rounded-md border-1 border-black font-medium  shadow-[0px_3px_0px_0px_rgba(0,0,0)] flex items-center justify-center gap-1 px-2 text-white hover:bg-purple-600 cursor-pointer'><HandHeart className='stroke-[1.5px]' />Donate</button>
-          <div className="userprofile flex items-center gap-3">
-            <span className="w-[40px] h-[40px] rounded-full border-1 border-black text-center flex items-center justify-center text-xl">
-              <User2Icon className='stroke-[1.5px]' />
-            </span>
-            <p className=''>Username</p>
+        <div className={`navMenu z-10 md:h-16 md:relative fixed md:top-0 flex flex-col md:flex-row items-center gap-10 bg-white md:bg-none md:w-auto w-full md:pb-0 pb-10 transition-all duration-300 ease-in-out md:shadow-none shadow-lg ${menuStatus?'top-14':'top-[-14rem]'}`}>
+          <div className="text-white md:w-80 w-70 md:h-11 h-12 md:mt-0 mt-8 flex items-center justify-around px-4 gap-2 searchBar bg-purple-500 hover:shadow-purple-500 shadow rounded-3xl">
+            <input className='outline-none w-full' value={value} onChange={(e) => setValue(e.target.value)} placeholder='Search' /> <Search onClick={searchLink} />
+          </div>
+          <div className='flex gap-4 items-center justify-center p-0 m-0 '>
+            <button className='h-[40px] w-[8rem] text-center bg-purple-500 outline-none rounded-md border-1 border-black font-medium  shadow-[0px_3px_0px_0px_rgba(0,0,0)] flex items-center justify-center gap-1 px-2 text-white hover:bg-purple-600 cursor-pointer'><HandHeart className='stroke-[1.5px]' />Donate</button>
+            <div className="userprofile flex items-center gap-3">
+              <span className="w-[40px] h-[40px] rounded-full border-1 border-black text-center flex items-center justify-center text-xl">
+                <User2Icon className='stroke-[1.5px]' />
+              </span>
+              <p className=''>Username</p>
+            </div>
           </div>
         </div>
       </nav>
