1b42282d4836c01d58286d96a3f0b345d8428eb0
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-22 23:18:34 +0530

    Added active button effect

--- Frontend/litly-frontend/src/components/DashboardButton.jsx
+++ Frontend/litly-frontend/src/components/DashboardButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DashboardButton = ({Icon, buttonTXT}) => {
   return (
-    <button className='flex justify-center items-center border-1 rounded-md h-10 px-3 text-lg text-white font-medium bg-purple-500 hover:bg-purple-600 cursor-pointer'>
+    <button className='flex justify-center items-center border-1 rounded-md h-10 px-3 text-lg text-white font-medium bg-purple-500 hover:bg-purple-600 active:bg-purple-500 cursor-pointer'>
         {Icon?<Icon />:'' } {buttonTXT}
     </button>
   )
