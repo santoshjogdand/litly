ba4bd78db8f834c50d92678835db309921a57a1d
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-25 16:12:08 +0530

    style changes

--- Frontend/litly-frontend/src/pages/Links.jsx
+++ Frontend/litly-frontend/src/pages/Links.jsx
@@ -8,16 +8,16 @@ function Links() {
             <h1 className='text-4xl font-semibold ' >Manage Links</h1>
             <p className='text-gray-700 ml-[2px]'>Manage all shortlinks here!</p>
           </div>
-          <div className="options flex md:justify-start justify-center gap-4 my-4 md:w-[40%] font-medium text-sm ">
-            <input type="date" placeholder='Filter by date' className='bg-white  border-1 rounded-lg h-10 md:p-4 px-2 w-full outline-none' id='dateFilter' />
-            <div className='border-1 rounded-lg h-10 md:p-4 px-2 w-full flex bg-white items-center justify-center' id='filter' >
+          <div className="options flex opacity-90 md:justify-start gap-4 my-4 md:w-[40%] w-full font-medium text-sm h-8 ">
+            <input type="date" placeholder='Filter by date' className='bg-white  border border-gray-200 rounded-lg md:p-4 px-1 outline-none' id='dateFilter' />
+            <div className='rounded-lg md:p-4 px-2 flex bg-white border border-gray-200 max-w-30 items-center justify-center' id='filter' >
              Add filters
             </div>
-            <div className='rounded-lg h-10 w-full flex items-center justify-center text-purple-600' id='filter' >
+            <div className='rounded-lg max-w-30 md:flex hidden items-center justify-center text-purple-600' id='filter' >
              Clear all
             </div>
           </div>
-          <hr className='opacity-50' />
+          <hr className='opacity-20' />
           <LinkComponent />
         </div>
   )
