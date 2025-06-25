d5b7e54bcad9556d90056acf5dd7ee75a55cbe0c
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-25 16:11:21 +0530

    changed background color

--- Frontend/litly-frontend/src/Layout/PrivateLayout.jsx
+++ Frontend/litly-frontend/src/Layout/PrivateLayout.jsx
@@ -12,7 +12,7 @@ function PrivateLayout() {
     <>
       <SideBar status={status} setStatus={setStatus} />
       <UserHeader />
-      <main className={`${status ? 'md:ml-60 ml-0' : 'md:ml-20 ml-0' }  pt-18 h-[100dvh] font-["Jost"] flex justify-center items-center overflow-auto scrollbar-hide bg-purple-100 transition-all ease-in-out duration-300`}>
+      <main className={`${status ? 'md:ml-60 ml-0' : 'md:ml-20 ml-0' }  pt-18 h-[100dvh] font-["Jost"] flex justify-center items-center overflow-auto scrollbar-hide bg-gray-100 transition-all ease-in-out duration-300`}>
         <Outlet />
       </main>
     </>
