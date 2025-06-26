392dbd2a940f97caa68abc77734cf8e93e53c9a4
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-26 22:31:46 +0530

    added bg

--- Frontend/litly-frontend/src/pages/Login.jsx
+++ Frontend/litly-frontend/src/pages/Login.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import { LogIn, CirclePlus } from 'lucide-react'
+import BlurredBg1 from '../components/BlurredBg1'
 
 function Login() {
   return (
     <div className='w-full min-h-screen flex justify-center relative items-center overflow-hidden'>
       
-      <svg 
-        preserveAspectRatio="xMidYMid slice" 
-        className='absolute inset-0 h-full w-full object-cover opacity-40 z-0'    
-        xmlns="http://www.w3.org/2000/svg" 
-        version="1.1"  
-        xmlnsXlink="http://www.w3.org/1999/xlink" 
-        viewBox="0 0 800 450"
-      >
-        <defs>
-          <filter id="bbblurry-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse">
-            <feGaussianBlur stdDeviation="87" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur>
-          </filter>
-        </defs>
-        <g filter="url(#bbblurry-filter)">
-          <ellipse rx="159" ry="115.5" cx="194.08515531285337" cy="412.0455123198438" fill="hsl(37, 99%, 67%)"></ellipse>
-          <ellipse rx="159" ry="115.5" cx="478.45510694412457" cy="388.1074124648792" fill="hsl(316, 73%, 52%)"></ellipse>
-          <ellipse rx="159" ry="115.5" cx="319.1566234287351" cy="473.95700409448483" fill="hsl(185, 100%, 57%)"></ellipse>
-        </g>
-      </svg>
+      <BlurredBg1 />
       
       <div className="flex min-h-screen flex-col z-10 backdrop-blur-md max-w-sm w-full py-10 rounded-3xl items-center justify-center px-8 gap-4">
         <div className="logo rounded-2xl shadow-lg shadow-purple-200 flex p-4 bg-white/80">
@@ -73,4 +56,4 @@ function Login() {
   )
 }
 
-export default Login
+export default React.memo(Login)
