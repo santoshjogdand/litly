ed10bf6233031c3a5edc40d3f104f4f46d519e0a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-27 16:01:58 +0000

    Fixed padding of guest header

--- Frontend/litly-frontend/src/components/GuestHeader.jsx
+++ Frontend/litly-frontend/src/components/GuestHeader.jsx
@@ -15,7 +15,7 @@ function GuestHeader() {
 
         </div>
         <div className={`transition-all z-10 md:h-0 h-[90dvh] w-full duration-300 menu flex md:flex-row flex-col md:w-auto md:static absolute md:top-0 top-14 md:translate-0 ${isActive ? `translate-y-[-60rem]` : ``} justify-start items-center text-purple-700 text-sm font-semibold`}>
-          <ul className='flex md:flex-row flex-col gap-4 justify-center items-center bg-white w-full py-10'>
+          <ul className='flex md:flex-row flex-col gap-4 justify-center items-center bg-white w-full md:py-0 py-10'>
             <a href="./about"><li className='cursor-pointer hover:bg-gray-100 transition-all duration-200 rounded-md px-4 py-2'>About</li></a>
             <li className='cursor-pointer hover:bg-gray-100 transition-all duration-200 rounded-md px-4 py-2'>Help</li>
             <li className='cursor-pointer hover:bg-gray-100 transition-all duration-200 rounded-md px-4 py-2'>FAQs</li>
