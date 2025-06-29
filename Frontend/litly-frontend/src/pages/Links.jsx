ac1f3625b77025613bf935935bbbb66c59ce2a7e
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-29 12:05:00 +0000

    added different screen sizes support

--- Frontend/litly-frontend/src/pages/Links.jsx
+++ Frontend/litly-frontend/src/pages/Links.jsx
@@ -3,23 +3,25 @@ import LinkComponent from '../components/LinkComponent'
 
 function Links() {
   return (
-        <div className='text-black h-full w-full p-10 font-medium'>
+    <>
+        <div className='container z-10 lg:h-[30rem] h-full w-full lg:rounded-lg backdrop-blur-4xl p-8 lg:pt-8 md:pt-25 pt-12  flex flex-col justify-center gap-6'>
           <div className="headings">
             <h1 className='text-4xl font-semibold ' >Manage Links</h1>
             <p className='text-gray-700 ml-[2px]'>Manage all shortlinks here!</p>
           </div>
-          <div className="options flex opacity-90 md:justify-start gap-4 my-4 md:w-[40%] w-full font-medium text-sm h-8 ">
-            <input type="date" placeholder='Filter by date' className='bg-white  border border-gray-200 cursor-pointer rounded-lg md:p-4 px-1 outline-none' id='dateFilter' />
-            <div className='rounded-lg md:p-4 px-2 flex bg-white border border-gray-200 max-w-30 items-center justify-center cursor-pointer' id='filter' >
+          <div className="options flex opacity-90 sm:justify-start gap-4 my-4 sm:w-[40%] w-full font-medium text-sm h-8 ">
+            <input type="date" placeholder='Filter by date' className='bg-white  border border-gray-200 cursor-pointer rounded-lg sm:p-4 px-1 outline-none' id='dateFilter' />
+            <div className='rounded-lg sm:p-4 px-2 flex bg-white border border-gray-200 max-w-30 items-center justify-center cursor-pointer' id='filter' >
              Add filters
             </div>
-            <div className='rounded-lg max-w-30 md:flex hidden items-center justify-center text-purple-600 cursor-pointer' id='filter' >
+            <div className='rounded-lg max-w-30 sm:flex hidden items-center justify-center text-purple-600 cursor-pointer' id='filter' >
              Clear all
             </div>
           </div>
           <hr className='opacity-20' />
           <LinkComponent />
         </div>
+        </>
   )
 }
 
