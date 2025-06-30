7084aaf5c1902226527d39ec12755282e22e712d
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-30 23:03:50 +0530

    removed unused imports

--- Frontend/litly-frontend/src/pages/Dashboard.jsx
+++ Frontend/litly-frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,4 @@
-import { Flame } from 'lucide-react';
-import React, { useState, useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import CustomShortModal from '../components/CustomShortModal';
 import DashboardButton from '../components/DashboardButton';
 
