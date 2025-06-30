7103ff47a8d731b484943a0c56e50057af774f8e
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-06-30 22:55:17 +0530

    added cors configurations

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,16 @@ import errorHandler from "./middlewares/errorHandler.middlerware.js"
 import router from "./routes/shortner.route.js";
 import cookieParser from "cookie-parser"
 import redirectRouter from "./routes/redirect.route.js"
+import cors from 'cors'
 
 const app = express()
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
+const corsOptions ={
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions))
 // endpoints
 //short-url
 //custom short-url
