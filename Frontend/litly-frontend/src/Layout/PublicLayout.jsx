9894bb5c1c73e9e8248f38eb6949558f1a9d2555
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-07-02 21:55:37 +0530

    Added redirection for already logged in users

--- Frontend/litly-frontend/src/Layout/PublicLayout.jsx
+++ Frontend/litly-frontend/src/Layout/PublicLayout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import GuestHeader from '../components/GuestHeader'
-import { Outlet } from 'react-router-dom'
+import { Outlet, Navigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
 
 function PublicLayout() {
+  const {isLoggedIn } = useAuth()
+  if(isLoggedIn){
+    return <Navigate to='/dashboard' replace/>
+  }
+
   return (
     <>
       <GuestHeader />
