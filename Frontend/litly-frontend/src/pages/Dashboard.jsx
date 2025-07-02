ae73d8f00d08e0e7a0dcaa7113f6e0e1040f3c6a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-07-02 21:56:16 +0530

    fixed stylings for dashboard

--- Frontend/litly-frontend/src/pages/Dashboard.jsx
+++ Frontend/litly-frontend/src/pages/Dashboard.jsx
@@ -11,10 +11,10 @@ function Dashboard() {
     }
   }, []);
   return (
-    <div className='text-black h-full md:w-[70%] w-full flex justify-center items-center'>
+    <>
       <svg preserveAspectRatio="xMidYMid slice" className='absolute inset-0 h-full w-full object-cover opacity-40 z-0' xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 800 800" opacity="0.64"><defs><filter id="bbblurry-filter" x="-100%" y="-100%" width="400%" height="400%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
         <feGaussianBlur stdDeviation="107" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" edgeMode="none" result="blur"></feGaussianBlur></filter></defs><g filter="url(#bbblurry-filter)"><ellipse rx="119" ry="123.5" cx="240.62248086929327" cy="600.6619189869274" fill="hsl(37, 99%, 67%)"></ellipse><ellipse rx="119" ry="123.5" cx="507.9660046100614" cy="551.430716384541" fill="hsl(316, 73%, 52%)"></ellipse><ellipse rx="119" ry="123.5" cx="363.59150023893875" cy="710.694238359278" fill="hsl(185, 100%, 57%)"></ellipse></g></svg>
-      <div className="container z-10 md:h-[30rem] h-full w-full md:rounded-lg backdrop-blur-4xl p-8 md:pt-8 pt-25 flex flex-col justify-center gap-6">
+      <div className="container z-10 md:h-[30rem] h-full w-full md:rounded-lg backdrop-blur-4xl p-8 lg:pt-8 md:pt-25 pt-12 flex flex-col justify-center gap-6">
         <div className="section1 flex flex-col ">
           <h1 className='md:text-4xl text-4xl font-semibold'>Create short links</h1>
           <p className=' text-sm font-medium text-gray-700 pl-1'>Quickly create random and custom short links.</p>
@@ -58,7 +58,7 @@ function Dashboard() {
         </div>
         <CustomShortModal className="hidden" />
       </div>
-    </div>
+    </>
   )
 }
 
