5595c9a5ded690929043f50ee4a19fbd9246e9fc
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-07-02 21:54:33 +0530

    added handling for secure routes

--- Frontend/litly-frontend/src/Layout/PrivateLayout.jsx
+++ Frontend/litly-frontend/src/Layout/PrivateLayout.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
 import UserHeader from '../components/UserHeader'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, replace } from 'react-router-dom'
 import SideBar from '../components/SideBar'
 import { useState } from 'react'
-
+import { useAuth } from '../context/AuthContext';
+import { LoaderCircle } from 'lucide-react'
 
 function PrivateLayout() {
   const [status, setStatus] = useState(false);
+  const { loading, isLoggedIn, user } = useAuth();
+  if (loading) {
+    return (
+      <div className='flex h-screen w-screen justify-center items-center'>
+        <LoaderCircle className='text-black animate-spin' />
+      </div>
+    )
+  }
+  if(!isLoggedIn){
+    console.log(isLoggedIn);
+    return <Navigate to="/login" replace/>
+  }
 
   return (
     <>
       <SideBar status={status} setStatus={setStatus} />
-      <UserHeader />
-      <main className={`${status ? 'md:ml-60 ml-0' : 'md:ml-20 ml-0' }  md:pt-18 pt-0 h-full font-["Jost"] flex justify-center items-center transition-all ease-in-out duration-300`}>
+      <UserHeader username={user?.username} />
+      <main className={`${status ? 'lg:ml-60 ml-0' : 'lg:ml-20 ml-0'} lg:pt-18 h-full flex-col pt-16 font-["Jost"] flex justify-center items-center transition-all ease-in-out duration-300`}>
         <Outlet />
       </main>
     </>
-  )
+  );
 }
 
 export default PrivateLayout
