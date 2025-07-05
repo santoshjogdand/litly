a9a4f94db7b7291f2a71ff1c7f81a944ae56c57c
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-07-05 12:02:00 +0000

    add logout route

--- Backend/routes/shortner.route.js
+++ Backend/routes/shortner.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, userInfo} from "../controllers/user.controller.js"
+import {registerUser, loginUser, userInfo, logoutUser} from "../controllers/user.controller.js"
 import { randomShortLink, customShortLink } from "../controllers/shortner.controller.js";
 import { getUrls, getUrlAnalytic, deleteUrl,updateUrl } from "../controllers/urls.controller.js";
 import auth from "../middlewares/auth.middleware.js";
@@ -16,5 +16,7 @@ router.route("/analytics/:id").get(auth,getUrlAnalytic)
 router.route("/urls/:id").put(auth, updateUrl).delete(auth, deleteUrl)
 router.route("/randomshort").post(auth,randomShortLink)
 router.route("/customshort").post(auth,customShortLink)
+
+router.route("/logout").get(auth,logoutUser)
 
 export default router
