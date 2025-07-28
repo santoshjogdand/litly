9cc96c7ee2a4a42da2198041f7221c64ae6ae305
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-07-28 15:41:21 +0000

    changed style of modal wrapper to inline block

--- Frontend/litly-frontend/src/components/ModalWrapper.jsx
+++ Frontend/litly-frontend/src/components/ModalWrapper.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const ModalWrapper = ({children}) => {
+const ModalWrapper = ({children, width}) => {
+    const Modalwidth = width?width:36
     return (
         <div className='fixed h-full w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  bg-black/50 z-10 overflow-hidden px-2 transition-all duration-300'>
-            <div className='relative max-w-[50rem] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded-lg bg-gray-100 p-10 flex flex-col gap-3 flex-grow'>
+            <div className={`relative w-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded-lg bg-gray-100 p-10 inline-block`}>
                 {children}
             </div>
         </div>
