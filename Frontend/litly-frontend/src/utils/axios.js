3113ffd05c65ec12921420efdd86ca2fea38ae2a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-08-02 22:43:37 +0530

    add backend url

--- Frontend/litly-frontend/src/utils/axios.js
+++ Frontend/litly-frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:80/api/v1',
+  baseURL: 'https://litly.vercel.app/api/v1',
   withCredentials: true, // ⬅️ Important for cookies
 });
 
