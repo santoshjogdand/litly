58386977d0282e8eb1aaf1db2ec767776f3a4b6a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-08-02 21:53:25 +0530

    just logging errors

--- Backend/utils/AsyncHandler.js
+++ Backend/utils/AsyncHandler.js
@@ -2,7 +2,9 @@ const asyncHandler = (requestHandler) =>{
     return (req, res, next) =>{
         Promise
         .resolve(requestHandler(req, res, next))
-        .catch((err) =>{ next(err)})
+        .catch((err) =>{
+            console.error(err);
+            next(err)})
     }
 }
 export default asyncHandler
