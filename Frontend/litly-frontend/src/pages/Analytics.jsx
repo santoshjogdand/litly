1536b7d552f4e8debfff4ec41aee8d09260aab7a
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-08-02 23:09:52 +0530

    DEV BUILD: 1.0

--- Frontend/litly-frontend/src/pages/Analytics.jsx
+++ Frontend/litly-frontend/src/pages/Analytics.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Chart from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2';
+import axios from '../utils/axios'
+
 
 function Analytics() {
+
+  useEffect(async ()=>{
+    await axios.get(`/analytics`)
+  },[])
+
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    const chartdata = {
+      labels: data.map(row => row.year),
+      datasets: [
+        {
+          label: 'Acquisitions by year',
+          data: data.map(row => row.count)
+        }
+      ]
+    }
+
+
   return (
-    <div>Analytics</div>
+    <div className='w-full h-screen flex justify-center '>
+      <h2 className='text-3xl font-semibold'>Analytics</h2>
+      <div id='acquisitions' className='Chart'>
+        <Bar data={chartdata} />
+      </div>
+    </div>
   )
 }
 
