2e04d93402dec996273d4163e3bedc95f58c67a9
Santosh Jogdand <santoshjogdand256@gmail.com>
2025-08-02 23:24:33 +0530

    changed to development

--- Frontend/litly-frontend/src/pages/Analytics.jsx
+++ Frontend/litly-frontend/src/pages/Analytics.jsx
@@ -6,37 +6,38 @@ import axios from '../utils/axios'
 
 function Analytics() {
 
-  useEffect(async ()=>{
-    await axios.get(`/analytics`)
-  },[])
+  // useEffect(async ()=>{
+  //   await axios.get(`/analytics`)
+  // },[])
 
-    const data = [
-      { year: 2010, count: 10 },
-      { year: 2011, count: 20 },
-      { year: 2012, count: 15 },
-      { year: 2013, count: 25 },
-      { year: 2014, count: 22 },
-      { year: 2015, count: 30 },
-      { year: 2016, count: 28 },
-    ];
+  //   const data = [
+  //     { year: 2010, count: 10 },
+  //     { year: 2011, count: 20 },
+  //     { year: 2012, count: 15 },
+  //     { year: 2013, count: 25 },
+  //     { year: 2014, count: 22 },
+  //     { year: 2015, count: 30 },
+  //     { year: 2016, count: 28 },
+  //   ];
 
-    const chartdata = {
-      labels: data.map(row => row.year),
-      datasets: [
-        {
-          label: 'Acquisitions by year',
-          data: data.map(row => row.count)
-        }
-      ]
-    }
+  //   const chartdata = {
+  //     labels: data.map(row => row.year),
+  //     datasets: [
+  //       {
+  //         label: 'Acquisitions by year',
+  //         data: data.map(row => row.count)
+  //       }
+  //     ]
+  //   }
 
 
   return (
     <div className='w-full h-screen flex justify-center '>
-      <h2 className='text-3xl font-semibold'>Analytics</h2>
+      {/* <h2 className='text-3xl font-semibold'>Analytics</h2>
       <div id='acquisitions' className='Chart'>
         <Bar data={chartdata} />
-      </div>
+      </div> */}
+      <h2>Under development</h2>
     </div>
   )
 }
