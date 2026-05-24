0975253dd5523eaadbfdc717209340b3485f0752
Santosh Jogdand <santoshjogdand256@gmail.com>
2026-05-25 01:55:23 +0530

    Comment out Chart.js imports in Analytics.jsx
    Commented out Chart.js imports for potential removal.

--- Frontend/litly-frontend/src/pages/Analytics.jsx
+++ Frontend/litly-frontend/src/pages/Analytics.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import Chart from 'chart.js/auto'
-import { Bar } from 'react-chartjs-2';
+// import Chart from 'chart.js/auto'
+// import { Bar } from 'react-chartjs-2';
 import axios from '../utils/axios'
 
 
